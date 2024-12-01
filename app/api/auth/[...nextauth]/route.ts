0f854550f93b9da92c7083ba51dfc7c72dbcc393
Z333Q <zeshan@natureofcommerce.com>
2024-12-01 16:12:27 +0400

    auth-layout.tsx and route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
 
 // Define authOptions inline with NextAuthOptions
-const authOptions: NextAuthOptions = {
+export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -7,11 +7,15 @@ export default async function AuthLayout({
 }: {
   children: React.ReactNode;
 }) {
+  // Use the getServerSession function to get the session
   const session = await getServerSession(authOptions);
 
+  // Redirect if the user is already signed in
   if (session) {
     redirect("/learn");
+    return null; // Ensure nothing is rendered after redirect
   }
 
+  // Render the children if the user is not signed in
   return <>{children}</>;
 }
