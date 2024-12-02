8039cfb2cbe5edc927c23047aac143afd5b8afae
Z333Q <zeshan@natureofcommerce.com>
2024-12-02 15:33:03 +0400

    auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,48 +1,6 @@
 
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import GithubProvider from "next-auth/providers/github";
+import { authOptions } from "../auth-config";
 
-// Configuration object for NextAuth
-const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-    }),
-    GithubProvider({
-      clientId: process.env.GITHUB_ID || "",
-      clientSecret: process.env.GITHUB_SECRET || "",
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-  session: {
-    strategy: "jwt",
-    maxAge: 30 * 24 * 60 * 60, // 30 days
-  },
-  pages: {
-    signIn: "/auth",
-    signOut: "/auth",
-    error: "/auth/error",
-    verifyRequest: "/auth/verify-request",
-  },
-  callbacks: {
-    async session({ session, token }) {
-      if (token && session.user) {
-        session.user.id = token.sub || "";
-      }
-      return session;
-    },
-    async jwt({ token, user, account }) {
-      if (account && user) {
-        token.accessToken = account.access_token;
-        token.userId = user.id;
-      }
-      return token;
-    },
-  },
-};
-
-// Export the NextAuth handler for GET and POST methods
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,21 +1,19 @@
+
 import { getServerSession } from "next-auth/next";
 import { redirect } from "next/navigation";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/api/auth/auth-config";
 
 export default async function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  // Use the getServerSession function to get the session
   const session = await getServerSession(authOptions);
 
-  // Redirect if the user is already signed in
   if (session) {
     redirect("/learn");
-    return null; // Ensure nothing is rendered after redirect
+    return null;
   }
 
-  // Render the children if the user is not signed in
   return <>{children}</>;
 }
