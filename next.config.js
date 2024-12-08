6c7511008b5b8c6a2ab6535a65e4835e0c423f26
Z333Q <zeshan@natureofcommerce.com>
2024-12-08 21:00:54 +0400

    next

--- next.config.js
+++ next.config.js
@@ -1,34 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: { 
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**.google.com'
-      },
-      {
-        protocol: 'https',
-        hostname: '**.facebook.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'open.spotify.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.pravatar.cc'
-      }
-    ]
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
+
+const path = require("path");
+
+module.exports = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
   },
 };
-
-module.exports = nextConfig;
