ed799818ed4a760f25c60276d42e30375f349afe
Z333Q <zeshan@natureofcommerce.com>
2024-12-09 16:38:13 +0400

    netify.toml

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
-
 const path = require("path");
 
 module.exports = {
   reactStrictMode: true,
+  experimental: {
+    appDir: true, // Enable experimental app directory support
+  },
   webpack: (config) => {
     config.resolve.alias["@"] = path.resolve(__dirname);
     return config;
