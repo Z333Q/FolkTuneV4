a7b649746ec79ffcbe0559fe09cf4e731ed44c5c
Z333Q <zeshan@natureofcommerce.com>
2024-12-09 16:46:40 +0400

    NEXT.CONFIG ERROR

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const path = require("path");
 
 module.exports = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true, // Enable experimental app directory support
-  },
   webpack: (config) => {
     config.resolve.alias["@"] = path.resolve(__dirname);
     return config;
