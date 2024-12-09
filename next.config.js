c11705467d884f7797950af0a5ebdaa97e2921e0
Z333Q <zeshan@natureofcommerce.com>
2024-12-09 18:36:46 +0400

    commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const path = require("path");
 
 module.exports = {
   reactStrictMode: true,
+  experimental: {
+    appDir: true, // Enable experimental app directory support
+  },
   webpack: (config) => {
     config.resolve.alias["@"] = path.resolve(__dirname);
     return config;
